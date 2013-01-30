48cb3006b11457a97de1185c5b7b6158c2d3cc7d
sleeplessninja <schafer.live@gmail.com>
2013-01-30 14:45:20 -0600

    Enable wireless Xbox 360 controller support
    Change-Id: Idc1513eacb31ba50f4df4cc61b16302287d195dc

--- product.mk
+++ product.mk
@@ -5,3 +5,7 @@ PRODUCT_PACKAGES += \
 # Lightning Browser for tuna
 PRODUCT_PACKAGES += \
     Lightning
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
